ced7e82617d4a19b960fd3f8f84b230c4e8e639f
İbrahim SEZER <ibrahimszr16@gmail.com>
2025-03-22 09:52:50 +0300

    Added recording in CSV format

--- lib/email_validator.dart
+++ lib/email_validator.dart
@@ -1,7 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-
+import 'package:csv/csv.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+
+Future<void> writeToCsv(List<List<dynamic>> rows, String filePath) async {
+  String csvData = const ListToCsvConverter().convert(rows);
+  final file = File(filePath);
+  await file.writeAsString(csvData);
+  print("Data saved in the $filePath");
+}
 
 bool isValidEmailFormat(String email) {
   final emailRegex =
@@ -19,8 +27,7 @@ Future<bool> isDomainValid(String email) async {
   }
 }
 
-Future<void> checkMxRecords(String email) async {
-  final domain = email.split('@').last;
+Future<List<String>> getMxRecords(String domain) async {
   final googleMX = "https://dns.google/resolve?name=$domain&type=MX";
   final url = Uri.parse(googleMX);
   final response = await http.get(url);
@@ -28,47 +35,64 @@ Future<void> checkMxRecords(String email) async {
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
     if (data['Answer'] != null) {
+      return data['Answer']
+          .map<String>((answer) => answer['data'] as String)
+          .toList();
+    }
+  }
+  return [];
+}
+
+Future<void> checkMailInfo(List<List<dynamic>> rows) async {
+  print("Email:");
+  String? email = stdin.readLineSync();
+  if (email != null && email != "") {
+    bool isEmailValid = isValidEmailFormat(email);
+    bool isDomainValidFlag = await isDomainValid(email);
+    List<String> mxRecords = await getMxRecords(email.split('@').last);
+    String status;
+
+    if (isEmailValid && isDomainValidFlag) {
+      status = "Email and Domain are valid";
+    } else if (isEmailValid) {
+      status = "Domain is not valid";
+    } else if (isDomainValidFlag) {
+      status = "Email is not valid";
+    } else {
+      status = "Email and Domain are not valid";
+    }
+
+    rows.add([email, status, mxRecords.join('; ')]);
+    print("\n$email\t\t\t# $status");
+
+    if (mxRecords.isNotEmpty) {
       print("\nMX results:");
-      for (var answer in data['Answer']) {
-        print(answer['data']);
-      }
+      mxRecords.forEach(print);
     } else {
       print("\nMX results not found.");
     }
-  } else {
-    print("Error: ${response.statusCode}");
-  }
-}
 
-Future<void> checkMailInfo() async {
-  print("Email:");
-  String? email = stdin.readLineSync();
-  if (email != null) {
-    if (isValidEmailFormat(email) && await isDomainValid(email)) {
-      print("\n$email\t\t\t# Email and Domain is valid ");
-    } else if (isValidEmailFormat(email) == true &&
-        await isDomainValid(email) != true) {
-      print("\n$email\t\t\t# Domain is not valid");
-    } else if (isValidEmailFormat(email) != true &&
-        await isDomainValid(email) == true) {
-      print("\n$email\t\t\t# Email is not valid");
-    } else {
-      print("\n$email\t\t\t# Email and Domain is not valid");
-    }
-    await checkMxRecords(email);
-    print("\nCheck another mail ? y or n");
+    print("\nCheck another mail? (y/n)");
     String? check = stdin.readLineSync();
-    if (check == "y") {
-      checkMailInfo();
+    if (check?.toLowerCase() == "y") {
+      await checkMailInfo(rows);
+    } else {
+      final dateTime = DateTime.now();
+      final formattedDateTime = DateFormat('yyyyMMdd_HHmm').format(dateTime);
+      final fileName = 'email_validation_results_$formattedDateTime.csv';
+      await writeToCsv(rows, fileName);
     }
   } else {
     print("Enter your email");
     print("\nRestarting...");
-    checkMailInfo();
+    await checkMailInfo(rows);
   }
 }
 
 //--------------------MAIN-----------------------//
 void main() async {
-  checkMailInfo();
+  List<List<dynamic>> rows = [
+    ['Email', "Status", "MX Records"]
+  ];
+  await checkMailInfo(rows);
 }
